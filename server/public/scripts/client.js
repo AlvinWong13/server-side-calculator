75c36c5f57364f390c0db05ce3e0274e28ad2a68
Alvin Wong <wong.alvin13@gmail.com>
2021-02-06 14:11:46 -0600

    append to dom and clear inputs. optain history of equations

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -24,28 +24,87 @@ function equate() {
     return false;
   }
   $.ajax({
-    method: 'POST'
+    method: 'POST',
     url: '/equate',
     data: {
       firstNumber: num1,
-      secondNumber: num2,
-      operator: operator
+      operator: operator,
+      secondNumber: num2
     }
   }).then(response => {
     console.log('equation response:', response);
-
+    getHistory();
   })
 }// end equate
 
+// check button input
+function buttonInput() {
+  console.log('button pressed');
+  let input = $(this).text();
+  if (input === '=' || input === 'C') {
+    return false;
+  }
+  console.log(input);
+  $('#calcInput').val($('#calcInput').val() + input);
+}// end button input
+
 // obtain history from server
+function getHistory() {
+  console.log('Get History');
+  $.ajax({
+    method: 'GET',
+    url: '/equate'
+  }).then((response) =>{
+    console.log('Retrieved history', response);
+    appendEquation(response);
+    $('#calcInput').val(`${response[response.length - 1].answer}`);
+  })
+}// response history
 
 // request server remove all previous data
+function clearHistory() {
+  console.log('clear history');
+  $.ajax({
+    method: 'Delete',
+    url: '/equate',
+    success: (result) => {
+      console.log('cleared history');
+      appendEquation(result);
+      clearInputs();
+      $('#recentHistory').text('');
+    }
+  })
+}// end history cleared
 
 // obtain operator
+function operatorClicked() {
+  console.log('obtain operator');
+  operator = $(this).text();
+  console.log(operator);
+}// end operator
 
 // post past equations to DOM
+function appendEquation(data) {
+  console.log('Append Equation');
+  $('#historyList').empty();
+  for ( const equations of data) {
+    $('#historyList').append(`
+    <li class="previousEquations">
+    ${equations.firstNumber}
+    ${equations.operator}
+    ${equations.secondNumber} 
+    = ${equations.answer}
+    </li>
+    `)
+  }
+}// end append Equation
 
 // empty previous inputs
+function clearInputs() {
+  console.log('clear inputs');
+  $('#calcInput').val('');
+  $('#recentHistory').text('')
+}// end clear inputs
 
 // check there are 2 inputs and an operator
 
